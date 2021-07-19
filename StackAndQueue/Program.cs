842021ce9fcf0b6befff68f0babe8f955c346901
charan_kanduri <charan2000kanduri@gmail.com>
2021-07-19 22:03:57 +0530

    [CHARAN]Stack Insert

--- StackAndQueue/Program.cs
+++ StackAndQueue/Program.cs
@@ -6,7 +6,17 @@ namespace StackAndQueue
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Creating object for stack and queue operations
+            StackAndQueueOperations stack = new StackAndQueueOperations();
+
+            //Push operations
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            //Display operation
+            Console.Write("\n-----DISPLAYING STACK ELEMENTS-----\n\n");
+            stack.Display();
+
         }
     }
 }
---
+++ StackAndQueue/StackAndQueueOperations.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+    class StackAndQueueOperations
+    {
+        Node top;
+
+        //Push data at Top
+        public void Push(int data)
+        {
+            Node newNode = new Node(data);
+            top = Add(newNode);
+        }
+
+        //Add data at top
+        public Node Add(Node newNode)
+        {
+            if (top == null)
+            {
+                top = newNode;
+            }
+            else
+            {
+                newNode.next = top;
+                top = newNode;
+            }
+            return top;
+        }
+        //Display stack data
+        public void Display()
+        {
+            Node temp = top;
+            int count = 0;
+            while (temp != null)
+            {
+                count++;
+                Console.WriteLine("Stack element at {0} is: {1}", count, temp.data);
+                temp = temp.next;
+            }
+        }
+    }
+}
